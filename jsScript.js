06373329c078edd2f08f18e4848403c429d884b1
AgustinhoIA <agustinhoigal@gmail.com>
2023-05-08 13:19:52 -0300

    Add incomplete gameOutcome function

--- jsScript.js
+++ jsScript.js
@@ -20,14 +20,46 @@ function playerChoice() {
             } else {
             break;
         }
-    } 
-    return pChoice;
+    } return pChoice;
 }
 
 
 /* Make a decision tree for the outcome of every match */
     /* A simple decision tree made of nested If */
+function gameOutcome() {
+    document.getElementById("pChoice").innerHTML = 'You choice is ' + pChoice;
+    document.getElementById('botChoice').innerHTML = 'PC choice is ' + bChoice;
+    switch(pChoice) {
 
+        case "rock":
+            if (bChoice == "scissors") {
+                document.getElementById('gameOutcome').innerHTML = 'Rock beats scissors: you win!'
+            } if (bChoice == pChoice) {
+                document.getElementById('gameOutcome').innerHTML = 'Draw!'
+            } else {
+                document.getElementById('gameOutcome').innerHTML = 'Paper beats rock: you loose!'
+            } break;
+
+        case "paper": 
+            if (bChoice == "rock") {
+                document.getElementById('gameOutcome').innerHTML = 'Paper beats rock: you win!'
+            } if (bChoice == pChoice) {
+                document.getElementById('gameOutcome').innerHTML = 'Draw!'
+            } else {
+                document.getElementById('gameOutcome').innerHTML = 'Scissors beats paper: you loose!'
+            } break;
+        case "scissors":
+            if (bChoice == "paper") {
+                document.getElementById('gameOutcome').innerHTML = 'Rock beats paper: you win!'
+            } if (bChoice == pChoice) {
+                document.getElementById('gameOutcome').innerHTML = 'Draw!'
+            } else {
+                document.getElementById('gameOutcome').innerHTML = 'Rock beats scissors: you loose!'
+            } break;
+            /* this fuction is still incomplete but Ive run out of time for today. 
+            I need to add the outcome as a quantifiable return*/
+    } return winner
+}
 /* display the players awnsers */
 /* display the result */
 /* display the winner */
